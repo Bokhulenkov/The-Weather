5c4c822c041e401a22f148b0347cd78fd4dc09f1
https://github.com/bokhulenkov/ <bokhulenkov@gmail.com>
2025-05-13 11:15:04 +0300

    Initial Commit

---
+++ The Weather/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  The Weather
+//
+//  Created by Alexander Bokhulenkov on 13.05.2025.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
