3750bd88a626ee51933d9507619f63b9dc2d6e1a
https://github.com/bokhulenkov/ <bokhulenkov@gmail.com>
2025-05-16 19:34:13 +0300

    feat [view]: cell without progress bar

--- The Weather/ViewController.swift
+++ The Weather/ViewController.swift
@@ -6,13 +6,133 @@
 //
 
 import UIKit
+import CoreLocation
 
 final class ViewController: UIViewController {
-
+    
+    //    MARK: - Properties
+    
+    private let network = NetworkService()
+    private var weather: Weather?
+    private var detailWeather: [DetailWeather] = []
+    private let weatherView = WeatherView()
+    private let countDay = 3
+    
+    //    MARK: - LifeCicle
+    
+    override func loadView() {
+        view = weatherView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        weatherView.setDelegate(self)
+        
+        //        network.fetchData(type: WeatherDTO.self, endPoint: .current(location: "LAT")) { result in
+        //            switch result {
+        //            case .success(let dto):
+        //                print(dto.location)
+        //                print(dto.current)
+        //                self.weather = Weather(from: dto)
+        //
+        //                DispatchQueue.main.async {
+        //                    self.weatherView.showWeather(from: self.weather)
+        //                }
+        //
+        //            case .failure(let error):
+        //                print(error.localizedDescription)
+        //                print(error.description)
+        //            }
+        //        }
+        
+        network.fetchData(type: WeatherDTO.self, endPoint: .days(countDay, location: "LAT")) { result in
+            switch result {
+            case .success(let dto):
+                self.detailWeather = dto.toDetailWeather()
+                self.weather = Weather(from: dto)
+                
+                DispatchQueue.main.async {
+                    self.weatherView.showWeather(from: self.weather)
+                }
+                
+            case .failure(let error):
+                print(error.localizedDescription)
+                print(error.description)
+            }
+        }
     }
-
-
 }
 
+
+//    MARK: - Extensions UITableViewDelegate, UITableViewDataSource
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return detailWeather.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: WeatherTableViewCell.identifier, for: indexPath) as? WeatherTableViewCell else {
+            print("Error created WeatherTableViewCell")
+            return UITableViewCell()
+        }
+        
+        var backgroundConfig = cell.backgroundConfiguration
+        backgroundConfig?.backgroundColor = .customBlue
+        backgroundConfig?.cornerRadius = 20
+        cell.backgroundConfiguration = backgroundConfig
+        cell.selectionStyle = .none
+        
+        let weather = detailWeather[indexPath.row]
+        
+        cell.configure(
+            timestamp: weather.day,
+            imageURL: weather.weatherImage,
+            minTemp: weather.minTemp,
+            maxTemp: weather.maxTemp
+        )
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        20
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        
+        let imageView: UIImageView = {
+            let imageView = UIImageView()
+            imageView.image = UIImage(systemName: "calendar")
+            imageView.tintColor = .systemBlue
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            return imageView
+        }()
+        
+        let titleLabel: UILabel = {
+            let label = UILabel()
+            label.text = "\(countDay)-Day Forecast"
+            label.font = UIFont.systemFont(ofSize: 12)
+            label.textColor = .systemBlue
+            label.translatesAutoresizingMaskIntoConstraints = false
+            return label
+        }()
+        
+        [
+            imageView,
+            titleLabel
+        ].forEach {headerView.addSubview($0)}
+        
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 16),
+            imageView.widthAnchor.constraint(equalToConstant: 12),
+            imageView.heightAnchor.constraint(equalToConstant: 12),
+            
+            titleLabel.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 8),
+            titleLabel.trailingAnchor.constraint(equalTo: headerView.trailingAnchor, constant: -16)
+        ])
+        
+        return headerView
+    }
+}
--- The Weather/WeatherTableViewCell.swift
+++ The Weather/WeatherTableViewCell.swift
@@ -5,4 +5,98 @@
 //  Created by Alexander Bokhulenkov on 15.05.2025.
 //
 
-import Foundation
+import UIKit
+
+final class WeatherTableViewCell: UITableViewCell {
+    
+    static let identifier = WeatherTableViewCell.description()
+    
+    private let day: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14)
+        label.textColor = .customGrey
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.tintColor = .customGrey
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let maxTemp: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14)
+        label.textColor = .customGrey
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let minTemp: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14)
+        label.textColor = .customGrey
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    //    MARK: - init
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUI()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //    MARK: - Methods
+    
+    private func setUI() {
+        [
+            day,
+            iconImageView,
+            maxTemp,
+            minTemp
+        ].forEach {contentView.addSubview($0)}
+    }
+    
+    func configure(timestamp: Int, imageURL: String, minTemp: Double, maxTemp: Double) {
+        self.day.text = Date.weekday(from: timestamp)
+        self.iconImageView.loadImage(from: imageURL)
+        self.maxTemp.text = "\(maxTemp)°"
+        self.minTemp.text = "\(minTemp)°"
+    }
+    
+    override func prepareForReuse() {
+        iconImageView.image = nil
+    }
+}
+
+//    MARK: - Extensions Constraints
+
+private extension WeatherTableViewCell {
+    func setConstraints() {
+        NSLayoutConstraint.activate([
+            day.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            day.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            day.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+            
+            iconImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 60),
+            iconImageView.heightAnchor.constraint(equalToConstant: 30),
+            iconImageView.widthAnchor.constraint(equalToConstant: 30),
+            iconImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            
+            minTemp.leadingAnchor.constraint(equalTo: iconImageView.trailingAnchor, constant: 25),
+            minTemp.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            
+            maxTemp.leadingAnchor.constraint(equalTo: minTemp.trailingAnchor, constant: 15),
+            maxTemp.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+    }
+}
